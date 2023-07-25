5892bb078077adc72f18c7be537244d32b7647b6
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2023-07-25 18:36:00 +0300

    Delete app

--- example/Program.cs
+++ example/Program.cs
@@ -5,8 +5,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddCordSdk(opt =>
 {
-    opt.ApplicationId = "45469abc-8288-4160-a8c3-bea9b7f07693";
-    opt.Secret = "e8786affb8466c3f0d937f6dcd37cde3b30fe1ba301122d2bde2f025c31b3bee";
+    opt.ApplicationId = "";
+    opt.Secret = "";
 });
 
 var app = builder.Build();
