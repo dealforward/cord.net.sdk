92c22fc6cbe469ee09e45eef0e0d05aa4ad0d131
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2023-07-25 15:53:53 +0300

    DOC: UserV1 docs added.

--- src/Cord.SDK/Application/Abstractions/Services/IUsersV1.cs
+++ src/Cord.SDK/Application/Abstractions/Services/IUsersV1.cs
@@ -8,8 +8,8 @@ public interface IUsersV1
     /// <summary>
     /// List all users from Cord. It uses the Rest API for this.
     /// </summary>
-    /// <param name="filter"></param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="filter">Request filtering</param>
+    /// <param name="cancellationToken">For Cancellation Request.</param>
     /// <returns>User List</returns>
     /// <exception cref="InvalidRequestException"></exception>
     /// <exception cref="UnexpectedFieldException"></exception>
@@ -28,18 +28,46 @@ public interface IUsersV1
     Task<IEnumerable<UserListItem>> List(UserListFilter? filter, CancellationToken cancellationToken);
 
     /// <summary>
-    /// Creates or updates a user record.
+    /// According to the Id value sent, if the user exists on the Cord, it is updated with the payload. If not, a new user record is created with payload and id.
     /// </summary>
-    /// <param name="id">Id value of the user record.</param>
+    /// <param name="id">User Id</param>
     /// <param name="item">User item</param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="cancellationToken">For Cancellation Request.</param>
     /// <returns>Cord Response</returns>
+    /// <exception cref="InvalidRequestException"></exception>
+    /// <exception cref="UnexpectedFieldException"></exception>
+    /// <exception cref="InvalidFieldException"></exception>
+    /// <exception cref="ApplicationNotFoundException"></exception>
+    /// <exception cref="OrganizationNotFoundException"></exception>
+    /// <exception cref="UserNotFoundException"></exception>
+    /// <exception cref="InvalidAuthorizationHeaderException"></exception>
+    /// <exception cref="MissingAuthorizationHeaderException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidCustomerTokenException"></exception>
+    /// <exception cref="InvalidAccessTokenException"></exception>
+    /// <exception cref="ExpiredAccessTokenException"></exception>
+    /// <exception cref="CordException">Generic unknown server error.</exception>
     Task<CordResponse?> UpdateOrCreate(string id, UserPutItem item, CancellationToken cancellationToken);
     /// <summary>
-    /// Finds a user
+    /// According to the Id value sent, the user's detail information is returned as nullable. 
     /// </summary>
-    /// <param name="id"></param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="id">User Id</param>
+    /// <param name="cancellationToken">For Cancellation Request.</param>
     /// <returns>User Item or null</returns>
+    /// <exception cref="InvalidRequestException"></exception>
+    /// <exception cref="UnexpectedFieldException"></exception>
+    /// <exception cref="InvalidFieldException"></exception>
+    /// <exception cref="ApplicationNotFoundException"></exception>
+    /// <exception cref="OrganizationNotFoundException"></exception>
+    /// <exception cref="UserNotFoundException"></exception>
+    /// <exception cref="InvalidAuthorizationHeaderException"></exception>
+    /// <exception cref="MissingAuthorizationHeaderException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidCustomerTokenException"></exception>
+    /// <exception cref="InvalidAccessTokenException"></exception>
+    /// <exception cref="ExpiredAccessTokenException"></exception>
+    /// <exception cref="CordException">Generic unknown server error.</exception>
     Task<UserGetItem?> Get(string id, CancellationToken cancellationToken);
 }
