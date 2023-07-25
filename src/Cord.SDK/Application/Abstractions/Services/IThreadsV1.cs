dec4f10ae7ca7cb18150e1c4dad1057f2cf97ebe
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2023-07-25 15:53:03 +0300

    DOC: ThreadsV1 docs added.

--- src/Cord.SDK/Application/Abstractions/Services/IThreadsV1.cs
+++ src/Cord.SDK/Application/Abstractions/Services/IThreadsV1.cs
@@ -4,7 +4,69 @@ namespace Cord.SDK;
 
 public interface IThreadsV1
 {
+    /// <summary>
+    /// Returns a list of all threads.
+    /// </summary>
+    /// <param name="cancellationToken">For Cancellation Request.</param>
+    /// <returns>List of threads</returns>
+    /// <exception cref="InvalidRequestException"></exception>
+    /// <exception cref="UnexpectedFieldException"></exception>
+    /// <exception cref="InvalidFieldException"></exception>
+    /// <exception cref="ApplicationNotFoundException"></exception>
+    /// <exception cref="OrganizationNotFoundException"></exception>
+    /// <exception cref="UserNotFoundException"></exception>
+    /// <exception cref="InvalidAuthorizationHeaderException"></exception>
+    /// <exception cref="MissingAuthorizationHeaderException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidCustomerTokenException"></exception>
+    /// <exception cref="InvalidAccessTokenException"></exception>
+    /// <exception cref="ExpiredAccessTokenException"></exception>
+    /// <exception cref="CordException">Generic unknown server error.</exception>
     Task<IEnumerable<ThreadListItem>> List(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns the Thread record as nullable according to the Id value sent.
+    /// </summary>
+    /// <param name="id">Thread Id</param>
+    /// <param name="cancellationToken">For Cancellation Request.</param>
+    /// <returns>Thread Item</returns>
+    /// <exception cref="InvalidRequestException"></exception>
+    /// <exception cref="UnexpectedFieldException"></exception>
+    /// <exception cref="InvalidFieldException"></exception>
+    /// <exception cref="ApplicationNotFoundException"></exception>
+    /// <exception cref="OrganizationNotFoundException"></exception>
+    /// <exception cref="UserNotFoundException"></exception>
+    /// <exception cref="InvalidAuthorizationHeaderException"></exception>
+    /// <exception cref="MissingAuthorizationHeaderException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidCustomerTokenException"></exception>
+    /// <exception cref="InvalidAccessTokenException"></exception>
+    /// <exception cref="ExpiredAccessTokenException"></exception>
+    /// <exception cref="CordException">Generic unknown server error.</exception>
     Task<ThreadGetItem?> Get(string id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates the Thread record according to the sent Id and Request Payload.
+    /// </summary>
+    /// <param name="id">Thread Id</param>
+    /// <param name="item">Request Payload</param>
+    /// <param name="cancellationToken"></param>
+    /// <exception cref="InvalidRequestException"></exception>
+    /// <exception cref="UnexpectedFieldException"></exception>
+    /// <exception cref="InvalidFieldException"></exception>
+    /// <exception cref="ApplicationNotFoundException"></exception>
+    /// <exception cref="OrganizationNotFoundException"></exception>
+    /// <exception cref="UserNotFoundException"></exception>
+    /// <exception cref="InvalidAuthorizationHeaderException"></exception>
+    /// <exception cref="MissingAuthorizationHeaderException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidCustomerTokenException"></exception>
+    /// <exception cref="InvalidAccessTokenException"></exception>
+    /// <exception cref="ExpiredAccessTokenException"></exception>
+    /// <exception cref="CordException">Generic unknown server error.</exception>
+    /// <returns cref="CordResponse?">Cord Response of operation</returns>
     Task<CordResponse?> Update(string id, ThreadPutItem item, CancellationToken cancellationToken);
 }
