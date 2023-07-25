c20382317dc7a36b672a96e158b0905043709497
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2023-07-25 15:53:22 +0300

    DOC: Token generator docs added.

--- src/Cord.SDK/Application/Abstractions/Services/ITokenGenerator.cs
+++ src/Cord.SDK/Application/Abstractions/Services/ITokenGenerator.cs
@@ -4,6 +4,16 @@ namespace Cord.SDK;
 
 public interface ITokenGenerator
 {
+    /// <summary>
+    /// Generates JWT Token for Rest API access with the configured `ApplicationId` and `Secret` information.
+    /// </summary>
+    /// <returns>JWT Token</returns>
     string GenerateServerToken();
+    
+    /// <summary>
+    /// It generates a JWT Token that users use in the WebSocket they connect to in order to participate in the Chat.
+    /// </summary>
+    /// <param name="user">User to connect to chat</param>
+    /// <returns>JWT Token</returns>
     string GenerateClientToken(CordAuthTokenUser user);
 }
