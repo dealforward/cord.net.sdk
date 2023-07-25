694b792f6941e1ae26c21d81c3f63ec7535ac7b1
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2023-07-25 15:52:36 +0300

    DOC: ThreadsContaner docs added.

--- src/Cord.SDK/Application/Abstractions/Services/IThreadsContainer.cs
+++ src/Cord.SDK/Application/Abstractions/Services/IThreadsContainer.cs
@@ -2,5 +2,9 @@ namespace Cord.SDK;
 
 public interface IThreadsContainer
 {
+    /// <summary>
+    /// REST API of version 1
+    /// </summary>
+    /// <value></value>
     IThreadsV1 V1 { get; }
 }
