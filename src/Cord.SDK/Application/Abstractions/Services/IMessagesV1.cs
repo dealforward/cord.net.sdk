4da606059782dcbcd64fb4921fc853da8d23e5d8
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2023-08-13 15:04:02 +0300

    2-messages-api: Get and Post requests have been added to the service

--- src/Cord.SDK/Application/Abstractions/Services/IMessagesV1.cs
+++ src/Cord.SDK/Application/Abstractions/Services/IMessagesV1.cs
@@ -5,7 +5,7 @@ namespace Cord.SDK;
 public interface IMessagesV1
 {
     /// <summary>
-    /// This endpoint returns information about all messages in a thread.
+    /// This method returns information about all messages in a thread.
     /// </summary>
     /// <param name="threadId">Thread Id</param>
     /// <param name="direction">Return messages in ascending or descending order of creation timestamp. 'descending' is the default.</param>
@@ -25,4 +25,48 @@ public interface IMessagesV1
     /// <exception cref="ExpiredAccessTokenException"></exception>
     /// <exception cref="CordException">Generic unknown server error.</exception>
     Task<IEnumerable<MessageListItem>> List(string threadId, MessageListShortDirection? direction, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// This method returns information about a single message.
+    /// </summary>
+    /// <param name="threadId">Thread Id</param>
+    /// <param name="direction">Return messages in ascending or descending order of creation timestamp. 'descending' is the default.</param>
+    /// <param name="cancellationToken"></param>
+    /// <exception cref="InvalidRequestException"></exception>
+    /// <exception cref="UnexpectedFieldException"></exception>
+    /// <exception cref="InvalidFieldException"></exception>
+    /// <exception cref="ApplicationNotFoundException"></exception>
+    /// <exception cref="OrganizationNotFoundException"></exception>
+    /// <exception cref="UserNotFoundException"></exception>
+    /// <exception cref="InvalidAuthorizationHeaderException"></exception>
+    /// <exception cref="MissingAuthorizationHeaderException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidCustomerTokenException"></exception>
+    /// <exception cref="InvalidAccessTokenException"></exception>
+    /// <exception cref="ExpiredAccessTokenException"></exception>
+    /// <exception cref="CordException">Generic unknown server error.</exception>
+    Task<MessageListItem?> Get(string threadId, string messageId, CancellationToken cancellationToken);
+    
+    /// <summary>
+    /// This method returns information about a single message.
+    /// </summary>
+    /// <param name="threadId">Thread Id</param>
+    /// <param name="item"></param>
+    /// <param name="cancellationToken"></param>
+    /// <exception cref="InvalidRequestException"></exception>
+    /// <exception cref="UnexpectedFieldException"></exception>
+    /// <exception cref="InvalidFieldException"></exception>
+    /// <exception cref="ApplicationNotFoundException"></exception>
+    /// <exception cref="OrganizationNotFoundException"></exception>
+    /// <exception cref="UserNotFoundException"></exception>
+    /// <exception cref="InvalidAuthorizationHeaderException"></exception>
+    /// <exception cref="MissingAuthorizationHeaderException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidAppTokenException"></exception>
+    /// <exception cref="InvalidCustomerTokenException"></exception>
+    /// <exception cref="InvalidAccessTokenException"></exception>
+    /// <exception cref="ExpiredAccessTokenException"></exception>
+    /// <exception cref="CordException">Generic unknown server error.</exception>
+    Task<CordResponse> Post(string threadId, MessagePostItem item, CancellationToken cancellationToken);
 }
--- src/Cord.SDK/Infrastructure/Services/MessagesV1.cs
+++ src/Cord.SDK/Infrastructure/Services/MessagesV1.cs
@@ -1,3 +1,5 @@
+using Cord.SDK.Application.Threads.V1.Commands.Message.Post;
+using Cord.SDK.Application.Threads.V1.Queries.Messages.Get;
 using Cord.SDK.Application.Threads.V1.Queries.Messages.List;
 using Cord.SDK.Objects;
 using MediatR;
@@ -10,5 +12,9 @@ internal sealed class MessagesV1 : IMessagesV1
 
     public MessagesV1(IMediator mediator) => _mediator = mediator;
 
+    public async Task<MessageListItem?> Get(string threadId, string messageId, CancellationToken cancellationToken) => await _mediator.Send(new MessagesGetRequest(threadId, messageId), cancellationToken);
+
     public async Task<IEnumerable<MessageListItem>> List(string threadId, MessageListShortDirection? direction, CancellationToken cancellationToken) => await _mediator.Send(new MessagesListRequest(threadId, direction), cancellationToken);
+
+    public async Task<CordResponse> Post(string threadId, MessagePostItem item, CancellationToken cancellationToken) => await _mediator.Send(new MessagePostRequest(threadId, item), cancellationToken);
 }
