cce83f5b814d954dc7dd8f5ad2307edb4da45094
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2023-09-16 21:57:04 +0300

    14-enums-naming-conversion: UserStatus enum changed

--- src/Cord.SDK/Objects/Users/UserStatus.cs
+++ src/Cord.SDK/Objects/Users/UserStatus.cs
@@ -1,10 +1,10 @@
 
+using System.Runtime.Serialization;
+
 namespace Cord.SDK.Objects;
 
 public enum UserStatus
 {
-    // ReSharper disable once InconsistentNaming
-    active,
-    // ReSharper disable once InconsistentNaming
-    deleted,
+    [EnumMember(Value = "active")] Active,
+    [EnumMember(Value = "deleted")] Deleted,
 }
