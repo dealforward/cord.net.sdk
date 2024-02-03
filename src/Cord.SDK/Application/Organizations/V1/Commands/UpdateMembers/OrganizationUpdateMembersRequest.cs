c9271d9a865160ea82b3dd9f45d0bacb0164583f
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2024-02-03 16:00:27 +0300

    23-organization-api-deprecated: OrganizationUpdateMembersRequest have marked as deprecated.

--- src/Cord.SDK/Application/Organizations/V1/Commands/UpdateMembers/OrganizationUpdateMembersRequest.cs
+++ src/Cord.SDK/Application/Organizations/V1/Commands/UpdateMembers/OrganizationUpdateMembersRequest.cs
@@ -3,5 +3,6 @@ using Cord.SDK.Objects;
 
 namespace Cord.SDK.Application.Organizations.V1.Commands.UpdateMembers;
 
+[Obsolete("Please use the group API instead of this.")]
 internal sealed record OrganizationUpdateMembersRequest
     (string Id, OrganizationUpdateMembers Members) : ICommand<IEnumerable<UserListItem>>;
