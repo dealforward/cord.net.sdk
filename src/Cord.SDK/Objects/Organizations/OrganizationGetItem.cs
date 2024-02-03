caf6edb0e215f02e56f0c73fba1853ffdec86456
Hanifi Yıldırımdağı <iletisim@hanifiyildirimdagi.com>
2024-02-03 15:59:00 +0300

    23-organization-api-deprecated: OrganizationGetItem model have marked as deprecated.

--- src/Cord.SDK/Objects/Organizations/OrganizationGetItem.cs
+++ src/Cord.SDK/Objects/Organizations/OrganizationGetItem.cs
@@ -1,9 +1,11 @@
 using Newtonsoft.Json;
 
 namespace Cord.SDK.Objects;
+
 /// <summary>
 /// Represents the HTTP request result for the organization detail.
 /// </summary>
+[Obsolete("Please use the group API instead of this.")]
 public sealed class OrganizationGetItem
 {
     [JsonProperty("id")] public string Id { get; set; } = null!;
